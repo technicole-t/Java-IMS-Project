01f29fe13bc86a4590c065df8955177393cac927
nicolethorley <thorleynicole@gmail.com>
2020-06-07 17:31:16 +0100

    local database connection completed

--- src/main/java/com/qa/inventoryms/dao/LocalConnection.java
+++ src/main/java/com/qa/inventoryms/dao/LocalConnection.java
@@ -1,5 +1,24 @@
 package com.qa.inventoryms.dao;
 
-public class LocalConnection {
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import com.qa.inventoryms.exceptions.ConnectionNotMade;
+
+public class LocalConnection extends DatabaseConnection {
+
+	public LocalConnection(String user, String password) {
+		super(user, password);
+	}
+	
+	public void openConnection()  {
+		// TO CHANGE FOR SQL EXCEPTION
+		try {
+			setConnection(DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/", getUsername(), getPassword()));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new ConnectionNotMade("Local database is not accessible: " + e.getMessage());
+		}		
+	}
 }
