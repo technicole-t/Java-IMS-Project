baadff58d8edab8eecd05ae3f11c654020fc2287
nicolethorley <thorleynicole@gmail.com>
2020-06-07 17:31:42 +0100

    remote database connection completed

--- src/main/java/com/qa/inventoryms/dao/RemoteConnection.java
+++ src/main/java/com/qa/inventoryms/dao/RemoteConnection.java
@@ -1,5 +1,27 @@
 package com.qa.inventoryms.dao;
 
-public class RemoteConnection {
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.qa.inventoryms.exceptions.ConnectionNotMade;
+
+public class RemoteConnection extends DatabaseConnection {
+	
+	public RemoteConnection(String user, String password) throws SQLException {
+		super(user, password);
+	}
+	
+	public void openConnection() {
+		// TO CHANGE SQLEXCEPTION LATER
+		try {
+			setConnection(DriverManager.getConnection("jdbc:mysql://34.67.87.1/inventorydb", getUsername(), getPassword()));;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new ConnectionNotMade("Remote database is not accessible: " + e.getMessage());
+
+		}		
+	}
+
 
 }
