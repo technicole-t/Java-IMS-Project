0b1d17c110585dcf068df3fd378a46af1d8db2ba
nicolethorley <thorleynicole@gmail.com>
2020-06-07 19:17:57 +0100

    added main method to Runner

--- src/main/java/com/qa/inventoryms/connect/Runner.java
+++ src/main/java/com/qa/inventoryms/connect/Runner.java
@@ -1,5 +1,13 @@
 package com.qa.inventoryms.connect;
 
+// IMPORT LOGGERS 
+
 public class Runner {
+	
+	// PUT LOGGER HERE
+	
+	public static void main(String[] args) {
+		IMS ims = new IMS();
+	}
 
 }
