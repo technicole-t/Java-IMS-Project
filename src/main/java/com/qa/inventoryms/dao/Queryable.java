548922f720adfbc64720d37161c063a804813f69
nicolethorley <thorleynicole@gmail.com>
2020-06-07 17:05:11 +0100

    Queryable Inferace complete

--- src/main/java/com/qa/inventoryms/dao/Queryable.java
+++ src/main/java/com/qa/inventoryms/dao/Queryable.java
@@ -1,5 +1,11 @@
 package com.qa.inventoryms.dao;
 
+import java.sql.ResultSet;
+
 public interface Queryable {
+	
+	public ResultSet sendQuery(String sql);
+	
+	public void sendUpdate(String sql);
 
 }
