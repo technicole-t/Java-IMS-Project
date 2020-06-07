31912e482aab7efd5ace7f5d70727b8556465f82
nicolethorley <thorleynicole@gmail.com>
2020-06-07 17:34:13 +0100

    exceptions created for connectionNotMade and SQLExceptions

---
+++ src/main/java/com/qa/inventoryms/exceptions/ConnectionNotMade.java
@@ -0,0 +1,21 @@
+package com.qa.inventoryms.exceptions;
+
+public class ConnectionNotMade extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String message;
+
+	public ConnectionNotMade(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+}
---
+++ src/main/java/com/qa/inventoryms/exceptions/SQLStatementException.java
@@ -0,0 +1,20 @@
+package com.qa.inventoryms.exceptions;
+
+public class SQLStatementException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String message;
+
+	public SQLStatementException(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
