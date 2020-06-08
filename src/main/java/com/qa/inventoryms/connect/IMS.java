786d09d5f1ecf968d9d0392e2bf18fcfb636fdc5
nicolethorley <thorleynicole@gmail.com>
2020-06-08 12:43:44 +0100

    IMS taking user input to insert customer into database

--- src/main/java/com/qa/inventoryms/connect/IMS.java
+++ src/main/java/com/qa/inventoryms/connect/IMS.java
@@ -1,5 +1,42 @@
 package com.qa.inventoryms.connect;
 
+
+import java.sql.SQLException;
+
+import com.qa.inventoryms.dao.CustomerDAO;
+import com.qa.inventoryms.dao.RemoteConnection;
+import com.qa.inventoryms.models.CustomerModel;
+import com.qa.inventoryms.utils.Input;
+
 public class IMS {
+	
+	Input input = new Input();
+
+	public void start() throws SQLException {
+
+		System.out.println("Database username: ");
+		String user = input.getInput();
+		System.out.println("Database password: ");
+		String password = input.getInput();
+
+		RemoteConnection connection = new RemoteConnection(user, password);
+		
+		System.out.println("customer name:");
+		String name = input.getInput();
+
+		System.out.println("customer email:");
+		String email = input.getInput();
+
+
+		CustomerModel customer = new CustomerModel(name, email, 0);
+		// ----------------------------
+		
+		// With the object send it to the Dao and have it do the rest
+		CustomerDAO customerDAO = new CustomerDAO(connection);
+		customerDAO.insertCustomer(customer);
+		
+		connection.closeConnection();
+	}
+
 
 }
