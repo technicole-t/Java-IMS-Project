eefba588688be25891bb3b6ddcfdf8d1ff842deb
nicolethorley <thorleynicole@gmail.com>
2020-06-08 12:44:07 +0100

    Runner able to start IMS

--- src/main/java/com/qa/inventoryms/connect/Runner.java
+++ src/main/java/com/qa/inventoryms/connect/Runner.java
@@ -1,13 +1,17 @@
 package com.qa.inventoryms.connect;
 
+import java.sql.SQLException;
+
 // IMPORT LOGGERS 
 
 public class Runner {
 	
 	// PUT LOGGER HERE
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		
 		IMS ims = new IMS();
+		ims.start();
 	}
 
 }
